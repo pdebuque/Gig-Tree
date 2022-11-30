8cc5ac59538fe5c65a701275742827937426c213
Paolo Debuque <pdebuque@gmail.com>
2022-11-30 13:55:53 -0600

    nav-bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,28 @@
-import {Button} from '@mui/material'
+import { Button, AppBar, Box, Toolbar, Typography, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu'
 
 export default function Nav() {
-    return(
-        <div>nav goes here
-            <Button>hello</Button>
-        </div>
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Gig manager
+                    </Typography>
+                    <Button color="inherit">Create Project</Button>
+                    <Button color="inherit">My Projects</Button>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
     )
 }
