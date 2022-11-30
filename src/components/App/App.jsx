cd617ddf0fac7ae44bac3152a86b5d2363a40c73
Paolo Debuque <pdebuque@gmail.com>
2022-11-30 12:55:04 -0600

    imported theme sheet

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,58 @@
+
+
+// component import
 import CreateProject from '../CreateProject/CreateProject';
 import DisplayProjects from '../DisplayProjects/DisplayProjects';
+import Nav from '../Nav/Nav';
 import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#1D3557',
+    },
+    secondary: {
+      main: '#da711f',
+    },
+    error: {
+      main: '#e63946',
+    },
+  },
+  typography: {
+    fontWeightLight: 300,
+    fontWeightBold: 700,
+    fontFamily: 'Raleway',
+    h1: {
+      fontWeight: 700,
+    },
+    body1: {
+      fontFamily: 'Merriweather',
+    },
+    body2: {
+      fontFamily: 'Merriweather',
+      lineHeight: 1.63,
+      fontSize: '0.8rem',
+    },
+    h5: {
+      lineHeight: 0.97,
+    },
+    h6: {
+      lineHeight: 1.12,
+    },
+  },
+})
 
 function App() {
   return (
-    <main>
-      <CreateProject />
-      <DisplayProjects />
-    </main>
+    <ThemeProvider theme={theme}>
+      <Nav />
+      <main>
+        <CreateProject />
+        <DisplayProjects />
+      </main>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,9 @@
+import {Button} from '@mui/material'
+
+export default function Nav() {
+    return(
+        <div>nav goes here
+            <Button>hello</Button>
+        </div>
+    )
+}
