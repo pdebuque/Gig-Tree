92afda1cfd00c3d5d14be98374c8392e07a8e3df
Paolo Debuque <pdebuque@gmail.com>
2022-12-03 14:15:23 -0600

    create temporary profile info, insert into projects

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,6 +3,7 @@ import { Grid } from '@mui/material'
 import ProfileProject from '../ProfileProject/ProfileProject';
 import { useState } from 'react';
 import TabPanel from '../TabPanel/TabPanel';
+import { projects, collaborators } from './temp-prof-info';
 
 export default function ProfileInfo() {
 
@@ -14,28 +15,34 @@ export default function ProfileInfo() {
     return (
         <div className="profile-data">
             <Grid container spacing={2}>
-                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <Tabs
-                        value={tabValue}
-                        onChange={changeTab}
-                    >
-                        <Tab label="all" />
-                        <Tab label="projects" />
-                        <Tab label="collaborators" />
-                    </Tabs>
-
-                </Box>
                 <Grid item xs={8}>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                        <Tabs
+                            value={tabValue}
+                            onChange={changeTab}
+                        >
+                            <Tab label="all" />
+                            <Tab label="projects" />
+                            <Tab label="collaborators" />
+                        </Tabs>
+
+                    </Box>
+
                     <Paper
                         elevation={1}
                     >
-                        <TabPanel value = {tabValue} index = {0}>
-                            ALL
+                        <TabPanel value={tabValue} index={0}>
+                            hello
                         </TabPanel>
-                        <TabPanel value = {tabValue} index = {1}>
-                            PROJECTS
+                        <TabPanel value={tabValue} index={1}>
+                            {projects.map(project => {
+                                return (
+                                    <ProfileProject key={project.id} project={project} />
+                                )
+                            })}
                         </TabPanel>
-                        <TabPanel value = {tabValue} index = {2}>
+                        <TabPanel value={tabValue} index={2}>
+                            {/* eventually: collaborators.map(collaborator=>return(<profilecollaborator collaborator = {collaborator}/>)) */}
                             COLLABORATORS
                         </TabPanel>
                     </Paper>
---
+++ src/components/Profile/ProfileInfo/temp-prof-info.js
@@ -0,0 +1,40 @@
+/* placeholder data pre-database */
+
+const _projects = [
+    {
+        id: 1,
+        name: 'Bridges',
+        ensemble: 'Minnesota Chorale',
+        location: 'Westminster Presbyterian',
+        owner: 'Kathy Romey'
+    },
+    {
+        id: 2,
+        name: 'Requiem',
+        ensemble: '',
+        location: 'Hamline University Methodist Church',
+        owner: 'Paolo Debuque'
+    }
+]
+
+
+const _collaborators = [
+    {
+        id: 1,
+        name: 'Kathy Romey',
+        about_blurb: 'Director of Choirs, University of Minnesota'
+    },
+    {
+        id: 2,
+        name: 'Jerry Blackstone',
+        about_blurb: 'Emeritus Director of Choirs, University of Michigan'
+    },
+    {
+        id: 3,
+        name: 'Carina Debuque',
+        about_blurb: 'alto and lover of musical theater'
+    }
+]
+
+export { _projects as projects };
+export { _collaborators as collaborators }
