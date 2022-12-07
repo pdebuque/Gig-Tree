f98e2b82c019d222ed62a3f087433c7968c23628
Paolo Debuque <pdebuque@gmail.com>
2022-12-07 09:58:58 -0600

    .sql and database structure diagram

--- git-tree.sql
+++ git-tree.sql
@@ -1,7 +1,7 @@
 
 -- primary tables: user, project, ensemble, date, piece
 
-CREATE TABLE user (
+CREATE TABLE "user" (
     id SERIAL PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
@@ -15,34 +15,36 @@ CREATE TABLE user (
     job_1 TEXT,
     job_1_location TEXT,
     job_2 TEXT,
-    job_2_location TEXT
-)
-
-CREATE TABLE project (
-    id SERIAL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    ensemble_id INTEGER REFERENCES ensemble,
-    owner_id INTEGER references user,
-    about TEXT,
-)
+    job_2_location TEXT,
+    prof_pic_path TEXT,
+    cover_pic_path TEXT
+);
 
 CREATE TABLE ensemble (
     id SERIAL PRIMARY KEY,
     director TEXT,
     about TEXT,
     website TEXT,
-    logo_path TEXT,
-)
+    logo_path TEXT
+);
 
--- all dates (rehearsals and performances)
+CREATE TABLE project (
+    id SERIAL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    ensemble_id INTEGER REFERENCES ensemble,
+    owner_id INTEGER references "user",
+    about TEXT
+);
+
+-- all events (rehearsals and performances)
 CREATE TABLE "event" (
     id SERIAL PRIMARY KEY,
     project_id INTEGER REFERENCES project,
     "location" TEXT,
     "date" DATE NOT NULL,
-    rehearsal BOOLEAN,
-    performance BOOLEAN, 
-)
+    "time" TIME NOT NULL,
+    rehearsal BOOLEAN
+);
 
 CREATE TABLE piece (
     id SERIAL PRIMARY KEY,
@@ -53,31 +55,31 @@ CREATE TABLE piece (
     arranger_last TEXT,
     "year" INTEGER,
     project_id INTEGER REFERENCES project,
-    notes TEXT,
-)
+    notes TEXT
+);
 
 -- junction tables: collaboration (user/user), user_project, user_ensemble(?)
 
 CREATE TABLE collaboration (
     id SERIAL PRIMARY KEY,
-    "user1_id" INTEGER REFERENCES user,
-    'user2_id' INTEGER REFERENCES user
-)
+    "user1_id" INTEGER REFERENCES "user",
+    "user2_id" INTEGER REFERENCES "user"
+);
 
 CREATE TABLE user_project (
     id SERIAL PRIMARY KEY,
-    "user_id" INTEGER REFERENCES user,
+    "user_id" INTEGER REFERENCES "user",
     project_id INTEGER REFERENCES project
-)
+);
 
 CREATE TABLE project_piece (
     id SERIAL PRIMARY KEY,
     project_id INTEGER REFERENCES project,
     piece_id INTEGER REFERENCES piece
-)
+);
 
 CREATE TABLE user_ensemble (
     id SERIAL PRIMARY KEY,
-    "user_id" INTEGER REFERENCES user,
+    "user_id" INTEGER REFERENCES "user",
     ensemble_id INTEGER REFERENCES ensemble
-)
+);
