de8f2aff7e45bd0e88f145ac88ed79341e0915df
Paolo Debuque <pdebuque@gmail.com>
2022-12-15 16:44:51 -0600

    redux infrastructure

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,16 +2,26 @@ import './Home.css';
 import { Grid, Typography, Paper, Avatar, Box } from '@mui/material'
 
 export default function Home() {
+
+
+  /* 
+  some backend mapping here...
+
+  projects will exist in projects.reducer.js, in src/redux/reducers
+
+  */
+
+
   return (
-
-
     <Box sx={{ width: '90%', marginX: 'auto', p: 1 }}>
       <Paper sx={{ p: 2, marginY: 1 }}>
         <Grid container spacing={1}>
+          <Grid item xs={.5}>
+            </Grid>
           <Grid item xs={1}>
-            <Avatar src='images/prof-pics/Paolo-prof-pic.png' alt='Paolo profile pic' sx={{width: '100%', height: '100%'}}/>
+            <Avatar src='images/prof-pics/Paolo-prof-pic.png' alt='Paolo profile pic' sx={{ width: 100, height: 100 }} />
           </Grid>
-          <Grid item xs={7}>
+          <Grid item xs={6}>
             <Typography variant='h2'>User Name</Typography>
             <Typography variant='subtitle1'>instrument 1, instrument 2</Typography>
           </Grid>
