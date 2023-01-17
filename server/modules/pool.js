cd7f955d5644c7f99dafa7654bf06796ea2af915
Paolo Debuque <pdebuque@gmail.com>
2023-01-17 15:39:05 -0600

    pool configure

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,16 +1,27 @@
 // Node Module that will connect to postgesql
 const pg = require('pg');
-
 // Setup PG to connect to the database
 const Pool = pg.Pool;
 
-const pool = new Pool({
-    database: 'gig-tree', // database name 
-    host: 'localhost', // where to find the database
-    port: 5432,        // port for finding the database
-    max: 10,           // max number of connections for the pool
-    idleTimeoutMillis: 30000 // 30 seconds before timeout/cancel query
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  pool = new pg.Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+          rejectUnauthorized: false
+      }
+  });
+}
+else {
+  pool = new pg.Pool({
+      host: 'localhost',
+      port: 5432,
+      database: 'gig-tree', 
+  });
+}
+
+
 
 // Listener setup on the pool isn't required, 
 // but can be super handy for troubleshooting.
